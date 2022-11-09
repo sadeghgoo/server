0b7ee7a5064909fd29f5bef4c24308a3711e1943
sadeghgoo <sadeghitunes2@gmail.com>
2022-11-09 22:50:00 +0330

    Add

--- Package.swift
+++ Package.swift
@@ -9,17 +9,16 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.0"),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "SwiftSoup", package: "SwiftSoup"),
             ],
             swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,94 @@
 import Vapor
+import SwiftSoup
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    let apiGroup = app.grouped("api")
+    apiGroup.on(.GET, "radioJavan") { request async throws in
+        let link = try request.query.decode(RadioJavanURLQuery.self)
+        guard let lastPartOfURL = link.link.split(separator: "/").last else {
+            throw Abort(.notAcceptable, reason: "Link is not valid.")
+        }
+        let response = try await request.client.get("https://rj-deskcloud.com/api2/mp3?id=\(lastPartOfURL)")
+        let musicModel = try response.content.decode(MusicModel.self)
+        
+        return RadioJavanMusicDownloadResponse(artistName: musicModel.artist, songName: musicModel.song, songURL: musicModel.link)
     }
+}
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+struct RadioJavanURLQuery: Content {
+    let link: String
+}
+
+struct RadioJavanMusicDownloadResponse: Content {
+    let artistName: String?
+    let songName: String?
+    let songURL: String?
+}
+
+// MARK: - MusicModel
+struct MusicModel: Content {
+    let id: Int?
+    let title: String?
+    let explicit: Bool?
+    let link: String?
+    let photo, thumbnail: String?
+    let plays, artist, song: String?
+    let photo240: String?
+    let likes, dislikes, downloads: String?
+    let photoPlayer: String?
+    let permlink: String?
+    let shareLink: String?
+    let createdAt: Date?
+    let type: MusicModelType?
+    let duration: Double?
+    let sampleStart: Int?
+    let hlsLink: String?
+    let lqLink, hqLink: String?
+    let lqHLS, hqHLS: String?
+    let lufs: Double?
+    let credits: String?
+    let creditTags, artistTags, bgColors: [String]?
+    let artistFarsi, songFarsi, date: String?
+    let related: [MusicModel]?
+    let lyric: String?
+    let lyricSynced: [LyricSynced]?
+    let allowSelfie: Bool?
+    let selfies: [Selfy]?
+}
+
+// MARK: - LyricSynced
+struct LyricSynced: Content {
+    let time: Double?
+    let text: String?
+}
+
+// MARK: - Selfy
+struct Selfy: Content {
+    let id: Int?
+    let hashID: String?
+    let title: String?
+    let mp3: Int?
+    let artist: String?
+    let song: String?
+    let hqLink, link: String?
+    let filename: String?
+    let shareLink: String?
+    let photo, thumbnail: String?
+    let verified: Bool?
+    let type: String?
+    let likes, likesPretty: String?
+    let hls, hdvc: String?
+    let user: User?
+    let location: String?
+}
+
+
+// MARK: - User
+struct User: Content {
+    let thumbnail: String?
+    let displayName, username: String?
+}
+
+enum MusicModelType: String, Content {
+    case mp3 = "mp3"
 }
